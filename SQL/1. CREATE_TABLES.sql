8c30712aa0723a7f8fb7ec6bcf42a8291fdc1955
Marco Rosales <Marco_Rosales@hotmail.es>
2020-02-25 23:37:58 -0600

    Initial commit

---
+++ SQL/1. CREATE_TABLES.sql
@@ -0,0 +1,15 @@
+DECLARE nCount  NUMBER;
+        v_sql   LONG;
+BEGIN
+    SELECT  COUNT(1) 
+      INTO  nCount 
+      FROM  DBA_TABLES 
+     WHERE  TABLE_NAME  = 'PRUEBA';
+     
+     IF(nCount <= 0) THEN     
+         v_sql  := 'CREATE TABLE PRUEBA(
+                        nombre  VARCHAR2(50))';
+
+        EXECUTE IMMEDIATE v_sql;
+    END IF;
+END;
